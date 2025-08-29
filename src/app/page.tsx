512b962c7d9e61ff22269bdd11637ec84bd48cfa
김해빈 <113007354+Kimheabin@users.noreply.github.com>
2025-08-30 01:33:07 +0900

    FEAT: Header 레이아웃/스타일 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
+import Header from "@/components/Header/Header";
+
 const Home = () => {
-  return <div>Home Page</div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 };
 
 export default Home;
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+import Image from "next/image";
+import { ReactNode } from "react";
+import styles from "./Header.module.css";
+
+type Props = {
+  rightBox?: ReactNode;
+};
+
+function Header({ rightBox }: Props) {
+  return (
+    <div className={styles.container}>
+      <div className={styles.inner}>
+        <div className={styles.left}>
+          <Link href="/" className={styles.logo}>
+            <Image
+              src="/assets/images/logo/white_log_md.svg" // public 폴더 기준
+              alt="헤더로고"
+              width={52}
+              height={12}
+            />
+          </Link>
+        </div>
+        <div className={styles.right}>{rightBox}</div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
