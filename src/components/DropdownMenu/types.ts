982cdf48203cab60a727df6ee1f05821d641440a
seunghyun <sseung30@gmail.com>
2025-09-01 21:37:06 +0900

    FEAT: DropdownMenu에 Size prop 추가

--- src/components/DropdownMenu/types.ts
+++ src/components/DropdownMenu/types.ts
@@ -6,4 +6,5 @@ export interface DropdownMenuItem {
 export interface DropdownMenuProps {
   children: React.ReactNode; // trigger 요소
   items: DropdownMenuItem[]; // 드롭다운 메뉴 아이템들
+  size: 'S' | 'M';
 }
