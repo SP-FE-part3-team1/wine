8cc20b752211893084028978f019dac3e4d56485
les0498 <iamsilverant@naver.com>
2025-09-01 23:00:54 +0900

    feat: CardMonthly 컴포넌트 제작(레이아웃 및 스타일 초안)

---
+++ src/app/wines/CardMonthly.tsx
@@ -0,0 +1,38 @@
+"use client";
+import { StarRating } from "@/components/StarRating";
+import styles from "./CardMonthly.module.css";
+
+type CardMonthlyProps = {
+  rating: number;
+  description?: string;
+  imageUrl?: string;
+  onClick?: () => void;
+};
+
+const CardMonthly = ({
+  rating,
+  description,
+  imageUrl,
+  onClick,
+}: CardMonthlyProps) => {
+  return (
+    <div className={styles.card} onClick={onClick}>
+      <div className={styles.body}>
+        {imageUrl && (
+          <div className={styles.imageWrapper}>
+            <img src={imageUrl} alt="와인 이미지" className={styles.image} />
+          </div>
+        )}
+        <div className={styles.content}>
+          <div className={styles.ratingWrapper}>
+            <span className={styles.ratingValue}>{rating.toFixed(1)}</span>
+            <StarRating value={rating} />
+          </div>
+          {description && <p className={styles.description}>{description}</p>}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CardMonthly;
