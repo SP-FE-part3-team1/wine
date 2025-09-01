bfc34cce5f25a839e0c2db5238013e588098333e
김해빈 <113007354+Kimheabin@users.noreply.github.com>
2025-09-01 16:21:26 +0900

    FIX: 반응형 구간 수정

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,5 +1,25 @@
+// const Home = () => {
+//   return <div>Home Page</div>;
+// };
+
+// export default Home;
+
+import Header from "@/components/Header/Header";
+import Profile from "@/components/Profile/Profile";
+
 const Home = () => {
-  return <div>Home Page</div>;
+  return (
+    <div>
+      <Header />
+      <div style={{ display: "flex", gap: "16px" }}>
+        <Profile
+          src="/assets/images/profile/profile.png"
+          size="lg"
+          // onClick={() => console.log("Small clicked")}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
