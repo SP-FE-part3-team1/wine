679316526190ccb1fbda6a4123e04b553e169c9e
seunghyun <sseung30@gmail.com>
2025-09-01 22:37:22 +0900

    FEAT: WineSummaryCard 추가

---
+++ src/app/wines/[id]/WineSummaryCard/WineSummaryCard.tsx
@@ -0,0 +1,25 @@
+import styles from './WineSummaryCard.module.css'
+import Tag from '../../../../components/Tag/Tag'
+
+type WineSummaryCardProps = {
+    imageUrl: string;
+    name: string;
+    origin: string;
+    price: string;
+}
+
+
+const WineSummaryCard = ({imageUrl, name, origin, price}: WineSummaryCardProps) => {
+  return(
+    <div className={styles.card}>
+      <img src={imageUrl} alt={`${name} wine bottle`} className={styles.wineImage} />
+      <div className={styles.cardContent}>
+        <h2 className={styles.name}>{name}</h2>
+        <p className={styles.origin}>{origin}</p>
+        <Tag>{price}</Tag>
+      </div>
+    </div>
+  );
+};
+
+export default WineSummaryCard
