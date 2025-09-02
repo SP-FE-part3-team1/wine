975ef84bf4e1190eed8d4a50e3c8c71fab6935b5
les0498 <iamsilverant@naver.com>
2025-09-02 16:28:27 +0900

    feat: CardWine 컴포넌트 제작(레이아웃 및 스타일 초안)

---
+++ src/stories/CardWine.stories.ts
@@ -0,0 +1,29 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import CardWine from "@/app/wines/CardWine";
+import "@/app/reset.css";
+
+const meta: Meta<typeof CardWine> = {
+  title: "Wines/CardWine",
+  component: CardWine,
+  tags: ["autodocs"],
+  argTypes: {
+    onClick: { action: "clicked" },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof CardWine>;
+
+export const Default: Story = {
+  args: {
+    imageUrl: "/assets/images/wine/wine1.png",
+    infoTitle: "Sentinel Cabernet Sauvignon 2016",
+    infoDescription: "Western Cape, South Africa",
+    tagLabel: "₩ 64,990",
+    rating: 4.8,
+    reviewState: "47개의 후기",
+    detailTitle: "최신 후기",
+    detailDescription:
+      "Cherry, cocoa, vanilla and clove - beautiful red fruit driven Amarone. Low acidity and medium tannins. Nice...",
+  },
+};
