ebd6dc2f847cef847b45caa9a29655c7b9f9bc04
limheewoo <heewoo39@gmail.com>
2025-09-03 08:35:01 +0900

    Fix: header를 전체 레이아웃에 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,8 @@ import "./globals.css";
 import "./reset.css";
 import style from "./layout.module.css";
 import { usePathname } from "next/navigation";
+import Header from "@/components/Header/Header";
+import Nav from "./Nav";
 
 const pretendard = localFont({
   src: "./fonts/PretendardVariable.woff2",
@@ -28,6 +30,11 @@ export default function RootLayout({
             pathname === "/login" || pathname === "/signup" ? "" : style.body
           }
         >
+          {!(pathname === "/login" || pathname === "/signup") && (
+            <div className={style.header}>
+              <Header rightBox={<Nav path={pathname} />} />
+            </div>
+          )}
           {children}
         </div>
         <div className="modal-root"></div>
