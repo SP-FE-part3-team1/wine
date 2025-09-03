55b7e8a7bd223bc72c50a194bac65a679093d8f7
김해빈 <113007354+Kimheabin@users.noreply.github.com>
2025-09-03 14:20:42 +0900

    Delete page.tsx

--- src/app/myprofile/page.tsx
+++ src/app/myprofile/page.tsx
@@ -1,18 +1,1 @@
-import Header from "@/components/Header/Header";
-import MyProfileCard from "../myprofile/_components/MyProfileCard";
-import MyProfileReveiwCard from "./_components/MyProfileReviewCard";
 
-const myprofile = () => {
-  return (
-    <div>
-      <Header />
-      <MyProfileCard
-        profileURL="/assets/images/profile/profile.png"
-        nickname="윤두준"
-      />
-      <MyProfileReveiwCard />
-    </div>
-  );
-};
-
-export default myprofile;
