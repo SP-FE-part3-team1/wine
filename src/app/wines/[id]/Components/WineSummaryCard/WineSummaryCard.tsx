f6a2a3fd7deab86d5d71dbdbbf032eeb79df9adb
seunghyun <sseung30@gmail.com>
2025-09-09 22:44:56 +0900

    FEAT: 와인 상세페이지 WineSummaryCard img를 next/Image로 교체

--- src/app/wines/[id]/Components/WineSummaryCard/WineSummaryCard.tsx
+++ src/app/wines/[id]/Components/WineSummaryCard/WineSummaryCard.tsx
@@ -1,5 +1,9 @@
+'use client';
+
 import styles from './WineSummaryCard.module.css'
 import Tag from '../../../../../components/Tag/Tag'
+import Image from 'next/image';
+import WineImage from '../WineImage';
 
 type WineSummaryCardProps = {
     imageUrl: string;
@@ -12,7 +16,14 @@ type WineSummaryCardProps = {
 const WineSummaryCard = ({imageUrl, name, origin, price}: WineSummaryCardProps) => {
   return(
     <div className={styles.card}>
-      <img src={imageUrl} alt={`${name} wine bottle`} className={styles.wineImage} />
+      <div className={styles.wineImageContainer}>
+      <Image 
+      src={imageUrl} 
+      alt={`${name} wine bottle`} 
+      fill
+      style={{ objectFit: 'cover' }}
+      />
+      </div>
       <div className={styles.cardContent}>
         <h2 className={styles.name}>{name}</h2>
         <p className={styles.origin}>{origin}</p>
